f82c7ddc23869c18f389fe287143d53bf5c8a028
lyg2 <ly.giasherwin@gmail.com>
2021-09-14 10:47:20 -0400

    Test 2
    test 2

--- exercice1.py
+++ exercice1.py
@@ -2,7 +2,7 @@ def fizzBuzz(n):
     # TODO imprimer la chaine de caractère appropriée avec la fonction print().
     #  Assigner ensuite la valeur à la variable resultat
 
-    resultat = jyfjhyf
+    resultat = wefwe
     return resultat
 
 if __name__ == '__main__':

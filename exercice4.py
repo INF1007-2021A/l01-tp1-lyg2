2ca0892294604a63915e7640aaf52ca00f270bab
MathieuBrulotte <mbrulott9@gmail.com>
2021-09-16 10:07:42 -0400

    Update exercice4.py

--- exercice4.py
+++ exercice4.py
@@ -3,9 +3,10 @@ import math
 
 def calculerPosition(positionInitiale, vitesseInitiale, duree, vitesseFinale):
     # TODO faites les calculs intermediaires, vous pouvez initialiser des variables locales.
-
+    vitesseInitiale0 = (vitesseInitiale * 1000) / (60*60)
+    vitesseFinale0 = (vitesseFinale * 1000) / (60 * 60)
     # TODO calculer la position finale, assigner la valeur à la variable "positionFinale"
-    positionFinale =
+    positionFinale = ((vitesseInitiale0 + vitesseFinale0) * duree)/2 + positionInitiale
 
     return positionFinale
 
@@ -14,4 +15,4 @@ if __name__ == '__main__':
     vitesseInitiale = int(input("Entrez la vitesse initiale en km/h: "))
     duree = int(input("Entrez la duree en secondes: "))
     vitesseFinale = int(input("Entrez la vitesseFinale en km/h: "))
-    print(calculerPosition(positionInitiale, vitesseInitiale, duree, vitesseFinale))
+    print('La position finale sera : ' + str(calculerPosition(positionInitiale, vitesseInitiale, duree, vitesseFinale)) + '.')
